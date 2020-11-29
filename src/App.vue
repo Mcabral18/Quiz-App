3aaaa709559f0f6cb436dd0a4c9303eeaa442cb6
Mcabral18 <cabralovil@gmail.com>
2020-11-29 21:50:28 +0000

    hgg

--- src/App.vue
+++ src/App.vue
@@ -33,14 +33,21 @@ export default {
     Header,
     QuestionBox,
   },
-  mounted() {
-    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple")
-      .then((response) => {
-        return response.json();
-      })
-      .then((Data) => {
-        this.questions = Data.results;
-      });
+  // mounted() {
+  //   fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((Data) => {
+  //       this.questions = Data.results;
+  //     });
+  // },
+  async mounted() {
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=27&type=multiple"
+    );
+    const data = await response.json();
+    this.questions = data.results;
   },
   methods: {
     //method to increment index value to change question
